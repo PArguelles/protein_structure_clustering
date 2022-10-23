a3e80f12915392328f107a9b33b7243eb13883ea
PArguelles <pedro.arguelles@gmail.com>
2022-10-23 23:14:14 +0100

    Delete ReplaceSymbols.py

--- OtherScripts/ReplaceSymbols.py
+++
@@ -1,9 +0,0 @@
-
-
-
-import os
-
-path = 'C:/ShareSSD/scop/kernels/'
-for filename in os.listdir(path):
-    parsed = str(filename).replace('.jpeg','').replace('_','-').replace('.','')
-    os.rename(path+filename,path+parsed+'.jpeg')
